86d5d76ae13dfb78dd50b7943557d61185343605
monchik <rapid.dima@gmail.com>
2024-10-14 21:56:54 +0500

    Добавил еще главную страницу по адресу /

--- less_5/urls.py
+++ less_5/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from books.views import HomePageView  # Импортируем представление главной страницы
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/', include('books.urls')),  # Подключаем маршруты из приложения books
+    path('books/', include('books.urls')),  # Подключаем маршруты для книг
+    path('', HomePageView.as_view(), name='home'),  # Главная страница
 ]
